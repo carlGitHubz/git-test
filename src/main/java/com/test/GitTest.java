5036ee0ba70045cfb9954ec6ae5fde09167a68db
Carl <395196884@qq.com>
2022-05-09 22:45:01 +0800

    第一次提交

---
+++ src/main/java/com/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @Author lyf
+ * @Date 2022-05-2022/5/9 22:41
+ */
+public class GitTest {
+  public static void main(String[] args) {
+      System.out.println("Hello Git");
+  }
+}
