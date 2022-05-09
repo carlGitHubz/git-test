9c91186195a3cf7dd51cf40e887111c939b9d28c
Carl <395196884@qq.com>
2022-05-09 22:46:39 +0800

    第二次提交

--- src/main/java/com/test/GitTest.java
+++ src/main/java/com/test/GitTest.java
@@ -7,5 +7,7 @@ package com.test;
 public class GitTest {
   public static void main(String[] args) {
       System.out.println("Hello Git");
+      System.out.println("Hello Git2");
+      System.out.println("Hello Git3");
   }
 }
